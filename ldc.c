c3a522d776ba03a62910be69075329e8c5b2af8b
clara95974 <annaelle.degard@laposte.net>
2021-12-29 17:46:22 +0100

    selection table avec verif + suppression element avec verif

--- ldc.c
+++ ldc.c
@@ -9,8 +9,9 @@
 
 int main(int argc,char** argv){
 //var
-int id_table;
-char* name_table;
+char name_table[10];
+char next;
+int error;
 
 connect_bdd();
 
@@ -20,10 +21,43 @@ printf("Ici, vous pouvez ajouter/modifier/supprimer une information \n");
 printf("Voici les TABLES que vous pouvez modifier\n");
 printf_tables();
 
-printf("Quelle table voulez-vous modifer ? (entrez son numero)\n");
-scanf("%d",&id_table);
+choose_table(name_table);//renvoie le nom de la table select
+scanf("%c",&next);
+if(next == 'n'){
+	do{
+		printf("Voulez-vous quitter (q) ou choisir une nouvelle table (c): ");
+		clean_stdin();
+		scanf("%c",&next);
+		if(next == 'q')
+                        return 0;
+		if(next == 'c')
+			break;
+		}while(next != 'q' || next != 'c');
 
+        if(next == 'c'){
+            clean_stdin();
+            choose_table(name_table);}//a countinuer
+}
+else if(next == 'o'){
+	printf("Voulez-vous ajouter (a),modifier (m) ou supprimer (s) un element ?: ");
+	clean_stdin();
+	scanf("%c",&next);
 
+	switch (next){
+		case 'a':
+			printf("a \n");
+			break;
+		case 'm':
+			printf("m \n");
+			break;
+		case 's':
+                        delete_element(name_table);
+                        break;
+		default://c pas encore quoi faire
+                        printf("erreur \n");
+                        break;
+	}
+}
 close_mysql();
 return 0;
 }
--- my_libsql.c
+++ my_libsql.c
@@ -3,27 +3,119 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include <unistd.h>
+
 MYSQL mysql;
 MYSQL_ROW row;
 
 //prototype
-void printf_tables();
+void my_fgets(char *string,int size);
+void clean_stdin();//remplace fflush
 
+void printf_tables();
+void choose_table(char* name_table);
+void delete_element(char* name_table);
 
 //func
 
+//func pour saisir strings
+void my_fgets(char *string,int size){
+	fgets(string,size,stdin);
+        if(string[strlen(string)-1]== '\n');
+        string[strlen(string)-1]= '\0';
+}
+
+void clean_stdin(void)
+{
+    int c;
+    do {
+        c = getchar();
+    } while (c != '\n' && c != EOF);
+}
+
 //print les TABLES de la bdd
 void printf_tables(){
+
 	int count=1;
 
         mysql_query(&mysql,"SHOW TABLES");// permit to do sql query => &mysql is pointer who ref at our bdd
         MYSQL_RES * res = mysql_store_result(&mysql);//store result in res =>&mysql is pointer who ref at our bdd
 
         while((row = mysql_fetch_row(res))) {
-         for (int i=0 ; i < mysql_num_fields(res); i++)
-            printf("\t %d) %s\n",count,row[i]);
-            count++;
+            printf("\t %d) %s\n",count,row[0]);
+		count++;
           }//print result to the request
 
-         mysql_free_result(res);//emply the buffer
+	mysql_free_result(res);//emply the buffer
+}
+
+//renvoie le nom de la table select
+void choose_table(char* name_table){
+int error=0;
+	do{
+		if(error==1){
+			printf("la table selectionnee n'existe pas \n");
+		}
+		printf("Quelle table voulez-vous modifer ? (entrez le nom de la table en majuscule)\n");
+		my_fgets(name_table,10);
+		char sql_cmd[2000];
+        	sprintf(sql_cmd,"SELECT * FROM %s",name_table);
+        	mysql_query(&mysql,sql_cmd);
+		MYSQL_RES * res = mysql_store_result(&mysql);
+		error=1;
+	}while((long) mysql_affected_rows(&mysql) == -1);
+
+	if((long) mysql_affected_rows(&mysql) == 0)
+		printf("aucune information n'est dans la table pour le moment\n");
+	else{
+		char sql_cmd[2000];
+                sprintf(sql_cmd,"SELECT * FROM %s",name_table);
+                mysql_query(&mysql,sql_cmd);
+                MYSQL_RES * res = mysql_store_result(&mysql);
+		printf("voici les informations contenues dans la table\n");
+                        while((row = mysql_fetch_row(res))) {
+                                for (int i=0 ; i < mysql_num_fields(res); i++)
+                                        printf("%s\n",row[i]);
+                        }
+		mysql_free_result(res);//emply the buffer
+		}
+		printf("Vous avez choisi la table %s. Continuez ? (o/n): ",name_table);
+}
+
+void  delete_element(char* name_table){
+int id;
+
+	char sql_cmd[2000];
+	sprintf(sql_cmd,"SELECT * FROM %s",name_table);
+	mysql_query(&mysql,sql_cmd);
+	MYSQL_RES * res = mysql_store_result(&mysql);
+
+	if((long) mysql_affected_rows(&mysql) == 0)
+                printf("aucune information a supprimer\n");
+	else{
+                sprintf(sql_cmd,"SELECT * FROM %s",name_table);
+                mysql_query(&mysql,sql_cmd);
+                MYSQL_RES * res = mysql_store_result(&mysql);
+ 		printf("voici les informations contenues dans la table\n");
+  		while((row = mysql_fetch_row(res))) {
+  			for (int i=0 ; i < mysql_num_fields(res); i++)
+  				printf("%s\n",row[i]);
+        	}
+  		mysql_free_result(res);//emply the buffer
+
+  		printf("Entrez l'ID de l'element a supprimer \n");
+  		clean_stdin();
+		scanf("%d",&id);
+		char sql_cmd[2000];
+  		sprintf(sql_cmd,"DELETE FROM %s WHERE id = %d",name_table,id);
+  		mysql_query(&mysql,sql_cmd);
+
+  		if((long) mysql_affected_rows(&mysql) == 0)
+                	printf("aucune information supprimes\n");
+  		else if((long) mysql_affected_rows(&mysql) == -1)
+                	printf("erreur lors de la suppression\n");
+  		else
+			printf("suppression effectuee avec succes");
+  	}
+
 }
