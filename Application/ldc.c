bfc2a5adaf88a82606e18a49fcf1a19feac550d1
clara95974 <annaelle.degard@laposte.net>
2022-01-09 12:27:02 +0100

    pour pull

--- Application/ldc.c
+++ Application/ldc.c
@@ -9,7 +9,7 @@ printf("Ici, vous pouvez ajouter/modifier/supprimer une information \n");
 
 printf("Voici les TABLES que vous pouvez modifier\n");
 printf_tables();
-
+clean_stdin();
 printf("Ou vous pouvez creer une nouvelle TABLE pour rajouter des informations\n");
 do{
 printf("Voulez-vous modifier une TABLE deja existante (m) ou en creer une nouvelle (a): ");
@@ -38,8 +38,10 @@ if(next == 'm'){
             choose_table(name_table);}//a countinuer
 	}
 	else if(next == 'o'){
+		clean_stdin();
 		printf("Voulez-vous ajouter (a),modifier (m) ou supprimer (s) un element ?: ");
 		scanf("%c",&next);
+		//clean_stdin();
 		switch (next){
 			case 'a':
 				insert_element(name_table);
