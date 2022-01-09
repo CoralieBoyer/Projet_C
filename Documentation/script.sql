1f0356a1e1964c6eefc95fda9b24758a0e6a492d
Coralie <coralie.byer@icloud.com>
2022-01-10 00:04:50 +0100

    modif sql

--- Documentation/script.sql
+++ Documentation/script.sql
@@ -24,8 +24,6 @@ CREATE TABLE EMPLOYE(  ID INTEGER AUTO_INCREMENT,  Nom VARCHAR(255),  Prenom VAR
 
 CREATE TABLE QUESTIONS(  ID INTEGER AUTO_INCREMENT,  Phrase VARCHAR(255), ID_EMPLOYE INTEGER,  ID_TRAVAIL INTEGER,  ID_SEXE INTEGER,  ID_YEUX INTEGER,  ID_CHEVEUX INTEGER,  ID_AGE INTEGER,  ID_TAILLE INTEGER,  ID_PILOSITE INTEGER,PRIMARY KEY (ID));
 
-CREATE TABLE FICHIER(  ID INTEGER AUTO_INCREMENT,  Nom VARCHAR(255),  Date_modif DATE,  ID_EMPLOYE INTEGER,PRIMARY KEY (ID));
-
 
 ALTER TABLE EMPLOYE ADD FOREIGN KEY (ID_ENTREPRISE) REFERENCES ENTREPRISE(ID);
 ALTER TABLE EMPLOYE ADD FOREIGN KEY (ID_TRAVAIL) REFERENCES TRAVAIL(ID);
@@ -46,3 +44,110 @@ ALTER TABLE QUESTIONS ADD FOREIGN KEY (ID_TAILLE) REFERENCES TAILLE(ID);
 ALTER TABLE QUESTIONS ADD FOREIGN KEY (ID_PILOSITE) REFERENCES PILOSITE(ID);
 
 ALTER TABLE FICHIER ADD FOREIGN KEY (ID_EMPLOYE) REFERENCES EMPLOYE(ID);
+
+
+
+INSERT INTO ENTREPRISE (Nom,Lien) VALUES("ESGI", "https://www.esgi.fr/");
+INSERT INTO ENTREPRISE (Nom,Lien) VALUES("ICAN", "https://www.ican-design.fr");
+
+INSERT INTO TRAVAIL (Domaine) VALUES("Professeur");
+INSERT INTO TRAVAIL (Domaine) VALUES("Eleve");
+INSERT INTO TRAVAIL (Domaine) VALUES("Administratif");
+
+INSERT INTO SEXE (Genre) VALUES("H");
+INSERT INTO SEXE (Genre) VALUES("F");
+INSERT INTO SEXE (Genre) VALUES("A");
+
+INSERT INTO YEUX (Couleur) VALUES("Marron");
+INSERT INTO YEUX (Couleur) VALUES("Bleu");
+INSERT INTO YEUX (Couleur) VALUES("Vert");
+
+INSERT INTO CHEVEUX (Couleur,Taille) VALUES("Brun", "Cours");
+INSERT INTO CHEVEUX (Couleur,Taille) VALUES("Brun", "Long");
+INSERT INTO CHEVEUX (Couleur,Taille) VALUES("Brun", "Mi-long");
+INSERT INTO CHEVEUX (Couleur,Taille) VALUES("Brun", "Carre");
+INSERT INTO CHEVEUX (Couleur,Taille) VALUES("Blond", "Cours");
+INSERT INTO CHEVEUX (Couleur,Taille) VALUES("Blond", "Long");
+INSERT INTO CHEVEUX (Couleur,Taille) VALUES("Blond", "Mi-long");
+INSERT INTO CHEVEUX (Couleur,Taille) VALUES("Blond", "Carre");
+INSERT INTO CHEVEUX (Couleur,Taille) VALUES("Chatain", "Cours");
+INSERT INTO CHEVEUX (Couleur,Taille) VALUES("Chatain", "Long");
+INSERT INTO CHEVEUX (Couleur,Taille) VALUES("Chatain", "Mi-long");
+INSERT INTO CHEVEUX (Couleur,Taille) VALUES("Chatain", "Carre");
+INSERT INTO CHEVEUX (Couleur,Taille) VALUES("Noir", "Cours");
+INSERT INTO CHEVEUX (Couleur,Taille) VALUES("Noir", "Long");
+INSERT INTO CHEVEUX (Couleur,Taille) VALUES("Noir", "Mi-long");
+INSERT INTO CHEVEUX (Couleur,Taille) VALUES("Noir", "Carre");
+INSERT INTO CHEVEUX (Couleur,Taille) VALUES("Blanc", "Cours");
+INSERT INTO CHEVEUX (Couleur,Taille) VALUES("Blanc", "Long");
+INSERT INTO CHEVEUX (Couleur,Taille) VALUES("Blanc", "Mi-long");
+INSERT INTO CHEVEUX (Couleur,Taille) VALUES("Blanc", "Carre");
+INSERT INTO CHEVEUX (Couleur,Taille) VALUES("Colore", "Cours");
+INSERT INTO CHEVEUX (Couleur,Taille) VALUES("Colore", "Long");
+INSERT INTO CHEVEUX (Couleur,Taille) VALUES("Colore", "Mi-long");
+INSERT INTO CHEVEUX (Couleur,Taille) VALUES("Colore", "Carre");
+INSERT INTO CHEVEUX (Couleur,Taille) VALUES("Aucun", "Chauve");
+
+INSERT INTO AGE (Age_min, Age_max) VALUES(15,20);
+INSERT INTO AGE (Age_min, Age_max) VALUES(20,25);
+INSERT INTO AGE (Age_min, Age_max) VALUES(25,30);
+INSERT INTO AGE (Age_min, Age_max) VALUES(30,35);
+INSERT INTO AGE (Age_min, Age_max) VALUES(35,40);
+INSERT INTO AGE (Age_min, Age_max) VALUES(40,45);
+INSERT INTO AGE (Age_min, Age_max) VALUES(45,50);
+INSERT INTO AGE (Age_min, Age_max) VALUES(50,55);
+INSERT INTO AGE (Age_min, Age_max) VALUES(55,60);
+INSERT INTO AGE (Age_min, Age_max) VALUES(60,65);
+INSERT INTO AGE (Age_min, Age_max) VALUES(65,70);
+
+INSERT INTO TAILLE (Taille_min, Taille_max) VALUES(120,130);
+INSERT INTO TAILLE (Taille_min, Taille_max) VALUES(130,140);
+INSERT INTO TAILLE (Taille_min, Taille_max) VALUES(140,150);
+INSERT INTO TAILLE (Taille_min, Taille_max) VALUES(150,160);
+INSERT INTO TAILLE (Taille_min, Taille_max) VALUES(160,170);
+INSERT INTO TAILLE (Taille_min, Taille_max) VALUES(170,180);
+INSERT INTO TAILLE (Taille_min, Taille_max) VALUES(180,190);
+INSERT INTO TAILLE (Taille_min, Taille_max) VALUES(190,200);
+
+--marche pas
+INSERT INTO PILOSITE (Barbe,Moustache,Taille,Couleur) VALUES('o', 'o', 'Grand', 'Noir');
+INSERT INTO PILOSITE (Barbe,Moustache,Taille,Couleur) VALUES('o', 'o', 'Petit', 'Noir');
+
+INSERT INTO QUESTIONS (Phrase,ID_TAILLE) VALUES('Est-ce que la personne fait entre 1m20 et 1m30 ?', 1);
+INSERT INTO QUESTIONS (Phrase,ID_TAILLE) VALUES('Est-ce que la personne fait entre 1m30 et 1m40 ?', 2);
+INSERT INTO QUESTIONS (Phrase,ID_TAILLE) VALUES('Est-ce que la personne fait entre 1m40 et 1m50 ?', 3);
+INSERT INTO QUESTIONS (Phrase,ID_TAILLE) VALUES('Est-ce que la personne fait entre 1m50 et 1m60 ?', 4);
+INSERT INTO QUESTIONS (Phrase,ID_TAILLE) VALUES('Est-ce que la personne fait entre 1m60 et 1m70 ?', 5);
+INSERT INTO QUESTIONS (Phrase,ID_TAILLE) VALUES('Est-ce que la personne fait entre 1m70 et 1m80 ?', 6);
+INSERT INTO QUESTIONS (Phrase,ID_TAILLE) VALUES('Est-ce que la personne fait entre 1m90 et 2m ?', 7);
+
+INSERT INTO QUESTIONS (Phrase,ID_AGE) VALUES('Est-ce que la personne a entre 15 et 20 ans ?', 1);
+INSERT INTO QUESTIONS (Phrase,ID_AGE) VALUES('Est-ce que la personne a entre 20 et 25 ans ?', 2);
+INSERT INTO QUESTIONS (Phrase,ID_AGE) VALUES('Est-ce que la personne a entre 25 et 30 ans ?', 3);
+INSERT INTO QUESTIONS (Phrase,ID_AGE) VALUES('Est-ce que la personne a entre 30 et 35 ans ?', 4);
+INSERT INTO QUESTIONS (Phrase,ID_AGE) VALUES('Est-ce que la personne a entre 35 et 40 ans ?', 5);
+INSERT INTO QUESTIONS (Phrase,ID_AGE) VALUES('Est-ce que la personne a entre 40 et 45 ans ?', 6);
+INSERT INTO QUESTIONS (Phrase,ID_AGE) VALUES('Est-ce que la personne a entre 45 et 50 ans ?', 7);
+INSERT INTO QUESTIONS (Phrase,ID_AGE) VALUES('Est-ce que la personne a entre 50 et 55 ans ?', 8);
+INSERT INTO QUESTIONS (Phrase,ID_AGE) VALUES('Est-ce que la personne a entre 55 et 60 ans ?', 9);
+INSERT INTO QUESTIONS (Phrase,ID_AGE) VALUES('Est-ce que la personne a entre 60 et 65 ans ?', 10);
+INSERT INTO QUESTIONS (Phrase,ID_AGE) VALUES('Est-ce que la personne a entre 65 et 70 ans ?', 11);
+
+--FAIRE CHEVEUX
+
+INSERT INTO QUESTIONS (Phrase,ID_YEUX) VALUES('Est-ce que la personne a des yeux marrons ?', 1);
+INSERT INTO QUESTIONS (Phrase,ID_YEUX) VALUES('Est-ce que la personne a des yeux bleus ?', 2);
+INSERT INTO QUESTIONS (Phrase,ID_YEUX) VALUES('Est-ce que la personne a des yeux verts ?', 3);
+
+INSERT INTO QUESTIONS (Phrase,ID_SEXE) VALUES('Est-ce que la personne est un homme ?', 1);
+INSERT INTO QUESTIONS (Phrase,ID_SEXE) VALUES('Est-ce que la personne est une femme ?', 2);
+INSERT INTO QUESTIONS (Phrase,ID_SEXE) VALUES('Est-ce que la personne se considere n appartenir a aucun genre ?', 3);
+
+INSERT INTO QUESTIONS (Phrase,ID_TRAVAIL) VALUES('Est-ce que la personne est un professeur ?', 1);
+INSERT INTO QUESTIONS (Phrase,ID_TRAVAIL) VALUES('Est-ce que la personne est un eleve ?', 2);
+INSERT INTO QUESTIONS (Phrase,ID_TRAVAIL) VALUES('Est-ce que la personne est un personnel administratif ?', 3);
+
+
+INSERT INTO EMPLOYE (Nom,Prenom,ID_ENTREPRISE,ID_TRAVAIL,ID_SEXE,ID_YEUX,ID_CHEVEUX,ID_AGE,ID_TAILLE) VALUES("Degard", "Annaelle",1,2,2);
+INSERT INTO EMPLOYE (Nom,Prenom,ID_ENTREPRISE,ID_TRAVAIL,ID_SEXE,ID_YEUX,ID_AGE,ID_TAILLE) VALUES("Boyer", "Coralie",1,2,2,1,1,4);
+INSERT INTO EMPLOYE (Nom,Prenom,ID_ENTREPRISE,ID_TRAVAIL,ID_SEXE,ID_YEUX,ID_CHEVEUX,ID_AGE,ID_TAILLE) VALUES("Sananes", "Frederique",1,2,1);
